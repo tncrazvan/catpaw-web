fc2533ec402fb4803ec8a3b678070783e35bf21f
Razvan <tangent.jotey@gmail.com>
2022-05-16 01:34:11 +0200

    Update StartWebServer.php

--- src/lib/Attributes/StartWebServer.php
+++ src/lib/Attributes/StartWebServer.php
@@ -20,12 +20,12 @@ class StartWebServer implements AttributeInterface {
 	use CoreAttributeDefinition;
 
 	/**
-	 * @param array|string              $interfaces list of interfaces to bind to.
-	 * @param array|string|false        $secureInterfaces list of secure interfaces to bind to (requires perCertificate).
-	 * @param string                    $webroot the directory the application should serve.
-	 * @param bool                      $showStackTrace if true the application will show the stack trace to the client.
-	 * @param bool                      $showExceptions if true the application will show exception messages to the client.
-	 * @param array<string,Certificate> $pemCertificates an array mapping domain names to pem certificates.
+	 * @param array|string                                $interfaces list of interfaces to bind to.
+	 * @param array|string|false                          $secureInterfaces list of secure interfaces to bind to (requires perCertificate).
+	 * @param string                                      $webroot the directory the application should serve.
+	 * @param bool                                        $showStackTrace if true the application will show the stack trace to the client.
+	 * @param bool                                        $showExceptions if true the application will show exception messages to the client.
+	 * @param array<string,array{file:string,key:string}> $pemCertificates an array mapping domain names to pem certificates (file and key).
 	 */
 	public function __construct(
 		public array|string       $interfaces = "127.0.0.1:8080",
@@ -41,7 +41,13 @@ class StartWebServer implements AttributeInterface {
 	public function main(LoggerInterface $logger): Promise {
 		return new LazyPromise(function() use ($logger) {
 			$config = new HttpConfiguration();
-			$config->pemCertificates = $this->pemCertificates;
+			$config->pemCertificates = [];
+
+			foreach($this->pemCertificates as $domain => $cert) {
+				$file = $cert['file']??'';
+				$key = $cert['key']??'';
+				$config->pemCertificates[$domain] = new Certificate($file, $key);
+			}
 
 			$config->httpInterfaces = $this->interfaces;
 			$config->httpSecureInterfaces = $this->secureInterfaces;
