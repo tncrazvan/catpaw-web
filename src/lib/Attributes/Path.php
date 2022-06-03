bfd6a2865a4622456b78c0e891e6043ecdd2fd6b
Razvan <tangent.jotey@gmail.com>
2022-06-03 18:43:07 +0200

    update

--- src/lib/Attributes/Path.php
+++ src/lib/Attributes/Path.php
@@ -45,23 +45,25 @@ class Path extends Service {
                 return;
             }
 
-            $filters = yield Filters::findByClass($reflection);
+            $filtersOfClass = yield Filters::findByClass($reflection);
 
-            $GET      = yield GET::findByClass($reflection);
-            $DELETE   = yield DELETE::findByClass($reflection);
-            $POST     = yield POST::findByClass($reflection);
-            $PUT      = yield PUT::findByClass($reflection);
-            $COPY     = yield COPY::findByClass($reflection);
-            $HEAD     = yield HEAD::findByClass($reflection);
-            $LINK     = yield LINK::findByClass($reflection);
-            $LOCK     = yield LOCK::findByClass($reflection);
-            $OPTIONS  = yield OPTIONS::findByClass($reflection);
-            $PATCH    = yield PATCH::findByClass($reflection);
-            $PROPFIND = yield PROPFIND::findByClass($reflection);
-            $PURGE    = yield PURGE::findByClass($reflection);
-            $UNKNOWN  = yield UNKNOWN::findByClass($reflection);
-            $UNLINK   = yield UNLINK::findByClass($reflection);
-            $UNLOCK   = yield UNLOCK::findByClass($reflection);
+            $CLASS_METHODS = [
+                'GET'      => yield GET::findByClass($reflection),
+                'DELETE'   => yield DELETE::findByClass($reflection),
+                'POST'     => yield POST::findByClass($reflection),
+                'PUT'      => yield PUT::findByClass($reflection),
+                'COPY'     => yield COPY::findByClass($reflection),
+                'HEAD'     => yield HEAD::findByClass($reflection),
+                'LINK'     => yield LINK::findByClass($reflection),
+                'LOCK'     => yield LOCK::findByClass($reflection),
+                'OPTIONS'  => yield OPTIONS::findByClass($reflection),
+                'PATCH'    => yield PATCH::findByClass($reflection),
+                'PROPFIND' => yield PROPFIND::findByClass($reflection),
+                'PURGE'    => yield PURGE::findByClass($reflection),
+                'UNKNOWN'  => yield UNKNOWN::findByClass($reflection),
+                'UNLINK'   => yield UNLINK::findByClass($reflection),
+                'UNLOCK'   => yield UNLOCK::findByClass($reflection),
+            ];
 
             /** @var Produces $producesOfClass */
             $producesOfClass = yield Produces::findByClass($reflection);
@@ -82,53 +84,34 @@ class Path extends Service {
                     $pathValue = preg_replace('/\/+/', '/', $classPathValue.$pathAttribute->getValue());
                 }
 
-
-                $GET_OF_METHOD      = ((yield GET::findByMethod($method))     ?? $GET);
-                $DELETE_OF_METHOD   = (yield DELETE::findByMethod($method))   ?? $DELETE;
-                $POST_OF_METHOD     = (yield POST::findByMethod($method))     ?? $POST;
-                $PUT_OF_METHOD      = (yield PUT::findByMethod($method))      ?? $PUT;
-                $COPY_OF_METHOD     = (yield COPY::findByMethod($method))     ?? $COPY;
-                $HEAD_OF_METHOD     = (yield HEAD::findByMethod($method))     ?? $HEAD;
-                $LINK_OF_METHOD     = (yield LINK::findByMethod($method))     ?? $LINK;
-                $LOCK_OF_METHOD     = (yield LOCK::findByMethod($method))     ?? $LOCK;
-                $OPTIONS_OF_METHOD  = (yield OPTIONS::findByMethod($method))  ?? $OPTIONS;
-                $PATCH_OF_METHOD    = (yield PATCH::findByMethod($method))    ?? $PATCH;
-                $PROPFIND_OF_METHOD = (yield PROPFIND::findByMethod($method)) ?? $PROPFIND;
-                $PURGE_OF_METHOD    = (yield PURGE::findByMethod($method))    ?? $PURGE;
-                $UNKNOWN_OF_METHOD  = (yield UNKNOWN::findByMethod($method))  ?? $UNKNOWN;
-                $UNLINK_OF_METHOD   = (yield UNLINK::findByMethod($method))   ?? $UNLINK;
-                $UNLOCK_OF_METHOD   = (yield UNLOCK::findByMethod($method))   ?? $UNLOCK;
+                $METHOD_METHODS = [
+                    'GET'      => yield GET::findByMethod($method),
+                    'DELETE'   => yield DELETE::findByMethod($method),
+                    'POST'     => yield POST::findByMethod($method),
+                    'PUT'      => yield PUT::findByMethod($method),
+                    'COPY'     => yield COPY::findByMethod($method),
+                    'HEAD'     => yield HEAD::findByMethod($method),
+                    'LINK'     => yield LINK::findByMethod($method),
+                    'LOCK'     => yield LOCK::findByMethod($method),
+                    'OPTIONS'  => yield OPTIONS::findByMethod($method),
+                    'PATCH'    => yield PATCH::findByMethod($method),
+                    'PROPFIND' => yield PROPFIND::findByMethod($method),
+                    'PURGE'    => yield PURGE::findByMethod($method),
+                    'UNKNOWN'  => yield UNKNOWN::findByMethod($method),
+                    'UNLINK'   => yield UNLINK::findByMethod($method),
+                    'UNLOCK'   => yield UNLOCK::findByMethod($method),
+                ];
 
                 /** @var Filters $filtersOfMethod */
-                $filtersOfMethod = (yield Filters::findByMethod($method)) ?? $filters;
+                $filtersOfMethod = yield Filters::findByMethod($method);
                 /** @var Produces $producesOfMethod */
                 $producesOfMethod = yield Produces::findByMethod($method);
                 /** @var Consumes $consumesOfMethod */
                 $consumesOfMethod = yield Consumes::findByMethod($method);
 
-
-                $inheritsProduces = !(bool)$producesOfMethod && (bool)$producesOfClass;
-                $inheritsConsumes = !(bool)$consumesOfMethod && (bool)$consumesOfClass;
-                
-                $exposed = (bool)$GET_OF_METHOD
-                || (bool)$DELETE_OF_METHOD
-                || (bool)$POST_OF_METHOD
-                || (bool)$PUT_OF_METHOD
-                || (bool)$COPY_OF_METHOD
-                || (bool)$HEAD_OF_METHOD
-                || (bool)$LINK_OF_METHOD
-                || (bool)$LOCK_OF_METHOD
-                || (bool)$OPTIONS_OF_METHOD
-                || (bool)$PATCH_OF_METHOD
-                || (bool)$PROPFIND_OF_METHOD
-                || (bool)$PURGE_OF_METHOD
-                || (bool)$UNKNOWN_OF_METHOD
-                || (bool)$UNLINK_OF_METHOD
-                || (bool)$UNLOCK_OF_METHOD;
-
                 $closureFilters = [];
-                if ($filtersOfMethod) {
-                    foreach ($filtersOfMethod->getClassNames() as $className) {
+                if ($filtersOfMethod || $filtersOfClass) {
+                    foreach (($filtersOfMethod ?? $filtersOfClass)->getClassNames() as $className) {
                         $reflectionFilterClass = new ReflectionClass($className);
                         if (!yield Service::findByClass($reflectionFilterClass)) {
                             $this->logger->warning("Class '{$reflectionFilterClass->getName()}' is not a valid filter wrapper because it is not annotated with '".Service::class."' or '".Singleton::class."', therefore it is being skipped.");
@@ -150,72 +133,28 @@ class Path extends Service {
                     }
                 }
 
+
+                $inheritsProduces = !(bool)$producesOfMethod && (bool)$producesOfClass;
+                $inheritsConsumes = !(bool)$consumesOfMethod && (bool)$consumesOfClass;
+                
+                
                 $closure = [...$closureFilters, $method->getClosure($instance)];
 
-                $methodValue = 'GET';
-
-                if ($GET_OF_METHOD) {
-                    yield Route::get($pathValue, $closure);
-                    $methodValue = 'GET';
-                }
-                if ($DELETE_OF_METHOD) {
-                    yield Route::delete($pathValue, $closure);
-                    $methodValue = 'DELETE';
-                }
-                if ($POST_OF_METHOD) {
-                    yield Route::post($pathValue, $closure);
-                    $methodValue = 'POST';
-                }
-                if ($PUT_OF_METHOD) {
-                    yield Route::put($pathValue, $closure);
-                    $methodValue = 'PUT';
-                }
-                if ($COPY_OF_METHOD) {
-                    yield Route::copy($pathValue, $closure);
-                    $methodValue = 'COPY';
-                }
-                if ($HEAD_OF_METHOD) {
-                    yield Route::head($pathValue, $closure);
-                    $methodValue = 'HEAD';
-                }
-                if ($LINK_OF_METHOD) {
-                    yield Route::link($pathValue, $closure);
-                    $methodValue = 'LINK';
-                }
-                if ($LOCK_OF_METHOD) {
-                    yield Route::lock($pathValue, $closure);
-                    $methodValue = 'LOCK';
-                }
-                if ($OPTIONS_OF_METHOD) {
-                    yield Route::options($pathValue, $closure);
-                    $methodValue = 'OPTIONS';
-                }
-                if ($PATCH_OF_METHOD) {
-                    yield Route::patch($pathValue, $closure);
-                    $methodValue = 'PATCH';
-                }
-                if ($PROPFIND_OF_METHOD) {
-                    yield Route::propfind($pathValue, $closure);
-                    $methodValue = 'PROPFIND';
-                }
-                if ($PURGE_OF_METHOD) {
-                    yield Route::purge($pathValue, $closure);
-                    $methodValue = 'PURGE';
-                }
-                if ($UNKNOWN_OF_METHOD) {
-                    yield Route::unknown($pathValue, $closure);
-                    $methodValue = 'UNKNOWN';
-                }
-                if ($UNLINK_OF_METHOD) {
-                    yield Route::unlink($pathValue, $closure);
-                    $methodValue = 'UNLINK';
-                }
-                if ($UNLOCK_OF_METHOD) {
-                    yield Route::unlock($pathValue, $closure);
-                    $methodValue = 'UNLOCK';
+                $methodValue = '';
+                $exposed     = false;
+                foreach ($METHOD_METHODS as $key => $value) {
+                    if ($value) {
+                        yield Route::custom($key, $pathValue, $closure);
+                        $exposed = true;
+                    } else {
+                        if ($CLASS_METHODS[$key] && ($producesOfMethod || $consumesOfMethod || $filtersOfMethod)) {
+                            yield Route::custom($key, $pathValue, $closure);
+                            $exposed = true;
+                        }
+                    }
                 }
 
-                if (!$exposed && ($consumesOfClass || $producesOfClass || $filtersOfMethod)) {
+                if (!$exposed && ($producesOfMethod || $consumesOfMethod || $filtersOfMethod)) {
                     yield Route::get($pathValue, $closure);
                     $exposed = true;
                 }
--- src/lib/Attributes/RequestHeader.php
+++ src/lib/Attributes/RequestHeader.php
@@ -8,7 +8,9 @@ use Attribute;
 use CatPaw\Attributes\Interfaces\AttributeInterface;
 use CatPaw\Attributes\Traits\CoreAttributeDefinition;
 use CatPaw\Web\HttpContext;
+use ReflectionIntersectionType;
 use ReflectionParameter;
+use ReflectionUnionType;
 
 #[Attribute]
 class RequestHeader implements AttributeInterface {
@@ -24,14 +26,23 @@ class RequestHeader implements AttributeInterface {
             &$value,
             $http
         ) {
-            $value = match ($reflection->getType()->getName()) {
+            $type = $reflection->getType();
+
+            if ($type instanceof ReflectionUnionType || $type instanceof ReflectionIntersectionType) {
+                $name = $type->getTypes()[0]->getName();
+            } else {
+                $name = $type->getName();
+            }
+            
+
+            $value = match ($name) {
                 'string' => $http->request->getHeader($this->key),
                 'bool'   => (bool)$http->request->getHeader($this->key),
                 'int'    => (int)$http->request->getHeader($this->key),
                 'double' => (double)$http->request->getHeader($this->key),
                 'float'  => (float)$http->request->getHeader($this->key),
                 'array'  => $http->request->getHeaderArray($this->key),
-                default  => $value,
+                default  => $http->request->getHeaderArray($this->key),
             };
         });
     }
