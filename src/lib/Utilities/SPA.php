77429b72ef6a9ad6409c75d912c9d700c958d142
Razvan Tanase <tangent.jotey@gmail.com>
2022-06-18 20:38:24 +0200

    Update SPA.php

--- src/lib/Utilities/SPA.php
+++ src/lib/Utilities/SPA.php
@@ -9,17 +9,30 @@ use CatPaw\Web\Attributes\Produces;
 use CatPaw\Web\Attributes\PUT;
 
 use CatPaw\Web\Attributes\RequestBody;
+use DomainException;
 
 abstract class SPA {
-    protected abstract function setState($state);
-    protected abstract function getState();
-    
+    protected abstract function setState(array $state);
+    protected abstract function getState():array;
+
+    private function isAssoc(array $arr) {
+        if ([] === $arr) {
+            return false;
+        }
+        return array_keys($arr) !== range(0, count($arr) - 1);
+    }
 
     #[GET]
     #[Path(":state")]
     #[Produces("application/json")]
-    public function get() {
-        return $this->getState();
+    public function get():array {
+        $state = $this->getState();
+
+        if (!$this->isAssoc($state)) {
+            throw new DomainException("All SPA states must be associative arrays.");
+        }
+
+        return $state;
     }
 
     #[PUT]
